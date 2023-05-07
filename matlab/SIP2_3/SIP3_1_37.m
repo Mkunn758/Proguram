0db35720b21c1e9250708b09d18767d7bd799f93
エム <129087083+Mkunn758@users.noreply.github.com>
2023-05-07 21:46:55 +0900

    Add files via upload

--- matlab/SIP2_3/SIP3_1_37.m
+++ matlab/SIP2_3/SIP3_1_37.m
@@ -1,26 +1,28 @@
 clear all;
 close all;
 
-f0 = 493.883; % シ4の周波数
-[msc, fs] = audioread('piano.mp3'); % ファイル名が正しくない場合は変更する
+f0 = 493.883;
+[msc, fs] = audioread('piano.mp3');
 t = 0:1/fs:length(msc)/fs;
 t = t(1:end-1);
+
+% ピアノの音の時間波形を描画
+figure();
+subplot(1,2,1);
+plot(t, msc);
+xlabel('[s]');
+title('時間波形');
 
 Wk = 0:(2*pi)/length(msc):2*pi;
 Wk = Wk(1:end-1);
 
+% 横軸を実際の周波数に直す
 Fs = fs*(Wk./(2*pi));
 
-figure();
-subplot(1,2,1);
-plot(t, msc);
-xlabel('(s)');
-title('時間波形');
-
+% ピアノの音の周波数振幅特性を描画
 Msc = fft(msc);
-
 subplot(1,2,2);
-plot(Fs,abs(Msc));
-axis([0 fs/2 0 7000]);
-xlabel('(Hz)');
+plot(Fs,abs(Msc))
+axis([0 Fs(length(Fs)/2) 0 7000])
+xlabel('[Hz]');
 title('振幅特性');
